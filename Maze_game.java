eddb409b220b2e0bfd626577ec9d8539f2036e21
talhayilmaztr <yilmaz.talha.tr@gmail.com>
2024-03-13 22:02:35 +0300

    Starting and ending points have been set

--- Maze_game.java
+++ Maze_game.java
@@ -1,26 +1,28 @@
+
 import java.util.Scanner;
 
 public class Maze_game {
+
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         char[][] maze = {
-                {'#', '!', '.', '.', 'R', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.'},
-                {'.', '.', '#', '.', '.', '.', '#', '.', 'H', '.', '.', '.', '.', '.', '!'},
-                {'F', '.', '.', '.', '#', '.', '!', '.', '.', 'R', '.', '.', '#', '#', '.'},
-                {'.', '.', '#', '.', '.', '#', '.', '.', '.', '.', 'F', '.', '.', '.', '.'},
-                {'.', '!', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '.', '.', '.'},
-                {'.', '.', 'H', '.', '.', '!', '.', '.', 'H', '.', '.', 'F', '.', '.', 'R'},
-                {'#', '#', '#', '#', '.', '.', '#', '.', '.', '.', 'T', '.', '.', '.', 'E'},
-                {'.', '.', '#', '.', 'F', '.', '#', '#', '.', '#', '#', '#', '#', '.', '.'},
-                {'.', '#', '.', '.', '.', '.', '!', '.', '#', '.', '.', '.', '#', '.', '.'},
-                {'.', 'T', 'T', '.', '#', '#', '.', '.', '.', '.', 'T', '.', '.', '.', 'R'},
-                {'.', '.', '.', '#', '.', '.', '.', '#', '.', '#', '.', '#', '.', 'T', '.'},
-                {'B', '.', '#', '.', '.', '!', '.', '!', '.', '.', '.', '.', '.', '.', '#'},
-                {'.', '.', '.', 'F', '!', '.', '.', '.', 'H', '.', '.', 'R', '.', '.', '.'},
-                {'.', '.', 'H', '.', '.', '.', '!', '.', '.', '.', '#', '.', '.', '#', '.'},
-                {'.', '.', 'H', '.', '.', '.', '!', '.', '.', '.', '#', '.', '.', '#', '.'},
-                {'.', '.', '.', '#', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '#'}};
+            {'#', '!', '.', '.', 'R', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.'},
+            {'.', '.', '#', '.', '.', '.', '#', '.', 'H', '.', '.', '.', '.', '.', '!'},
+            {'F', '.', '.', '.', '#', '.', '!', '.', '.', 'R', '.', '.', '#', '#', '.'},
+            {'.', '.', '#', '.', '.', '#', '.', '.', '.', '.', 'F', '.', '.', '.', '.'},
+            {'.', '!', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '.', '.', '.'},
+            {'.', '.', 'H', '.', '.', '!', '.', '.', 'H', '.', '.', 'F', '.', '.', 'R'},
+            {'#', '#', '#', '#', '.', '.', '#', '.', '.', '.', 'T', '.', '.', '.', 'E'},
+            {'.', '.', '#', '.', 'F', '.', '#', '#', '.', '#', '#', '#', '#', '.', '.'},
+            {'.', '#', '.', '.', '.', '.', '!', '.', '#', '.', '.', '.', '#', '.', '.'},
+            {'.', 'T', 'T', '.', '#', '#', '.', '.', '.', '.', 'T', '.', '.', '.', 'R'},
+            {'.', '.', '.', '#', '.', '.', '.', '#', '.', '#', '.', '#', '.', 'T', '.'},
+            {'B', '.', '#', '.', '.', '!', '.', '!', '.', '.', '.', '.', '.', '.', '#'},
+            {'.', '.', '.', 'F', '!', '.', '.', '.', 'H', '.', '.', 'R', '.', '.', '.'},
+            {'.', '.', 'H', '.', '.', '.', '!', '.', '.', '.', '#', '.', '.', '#', '.'},
+            {'.', '.', 'H', '.', '.', '.', '!', '.', '.', '.', '#', '.', '.', '#', '.'},
+            {'.', '.', '.', '#', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '#'}};
 
         int end_row = 0;
         int end_col = 0;
@@ -30,7 +32,6 @@ public class Maze_game {
         int[] player_column = new int[1];
 
         int[] counter = new int[1];
-
 
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
@@ -50,31 +51,35 @@ public class Maze_game {
                 }
             }
         }
+        player_row[0] = start_row;
+        player_column[0] = start_col;
+        System.out.println("Maze Game:");
+
         boolean isGameRunning = true;
         while (isGameRunning) {
             printMaze(maze);
             System.out.println("Your step count: " + counter[0]);
-System.out.println("Your current position: " + player_row[0] + "." + player_column[0]);
-System.out.println("Enter one of the characters W, A, S, D to move ");
-String move = input.next();
-switch (move) {
-    case "w":
-        up(counter, maze, player_row, player_column);
-        break;
-    case "s":
-        down(counter, maze, player_row, player_column);
-        break;
-    case "a":
-        left(counter, maze, player_row, player_column);
-        break;
-    case "d":
-        right(counter, maze, player_row, player_column);
-        break;
+            System.out.println("Your current position: " + player_row[0] + "." + player_column[0]);
+            System.out.println("Enter one of the characters W, A, S, D to move ");
+            String move = input.next();
+            switch (move) {
+                case "w":
+                    up(counter, maze, player_row, player_column);
+                    break;
+                case "s":
+                    down(counter, maze, player_row, player_column);
+                    break;
+                case "a":
+                    left(counter, maze, player_row, player_column);
+                    break;
+                case "d":
+                    right(counter, maze, player_row, player_column);
+                    break;
 
+            }
+
+        }
     }
-        
-}
-}
 
     public static void printMaze(char maze[][]) {
         for (int i = 0; i < maze.length; i++) {
@@ -85,6 +90,7 @@ switch (move) {
         }
         System.out.println();
     }
+
     public static void up(int counter[], char maze[][], int player_row[], int player_column[]) {
         if (player_row[0] > 0) {
             player_row[0]--;
@@ -94,7 +100,7 @@ switch (move) {
             System.out.println("Cannot move up!");
         }
     }
-    
+
     public static void down(int counter[], char maze[][], int player_row[], int player_column[]) {
         if (player_row[0] < 14) {
             player_row[0]++;
@@ -104,7 +110,7 @@ switch (move) {
             System.out.println("Cannot move down!");
         }
     }
-    
+
     public static void right(int counter[], char maze[][], int player_row[], int player_column[]) {
         if (player_column[0] < 14) {
             player_column[0]++;
@@ -114,7 +120,7 @@ switch (move) {
             System.out.println("Cannot move right!");
         }
     }
-    
+
     public static void left(int counter[], char maze[][], int player_row[], int player_column[]) {
         if (player_column[0] > 0) {
             player_column[0]--;
@@ -124,7 +130,5 @@ switch (move) {
             System.out.println("Cannot move left!");
         }
     }
-    
-    
-    
+
 }
